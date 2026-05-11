953a81aa6e21714b94f078daa2701963fd79b4ec
Nico Devs <nico1485@gmail.com>
2026-05-11 15:46:31 -0300

    respect base url when resolving stamp asset paths

--- components/PolaroidStamp.vue
+++ components/PolaroidStamp.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import type { StampSide } from '../types'
 
-defineProps<{
+const props = defineProps<{
   url?: string
   position?: StampSide
 }>()
+
+const resolvedUrl = computed(() => {
+  if (!props.url) return ''
+  if (props.url.startsWith('/')) {
+    return `${import.meta.env.BASE_URL}${props.url.slice(1)}`
+  }
+  return props.url
+})
 </script>
 
 <template>
   <div v-if="url" :class="['mumbo-polaroid size-28', position || 'tr']">
     <div class="mumbo-polaroid-frame" />
-    <img class="mumbo-polaroid-logo" :src="url" alt="" />
+    <img class="mumbo-polaroid-logo" :src="resolvedUrl" alt="" />
   </div>
 </template>
